50ce22a7bfec173bb8ec92b8d0719ba31b975d6a
root <root@laus02.agi.tsn>
2020-12-21 10:14:05 +0000

    fix pathes for Desktoplink for Testees

--- laus/scriptsForClasses/CONFIG_U2004/files/makeDesktopLinkForTestees.sh
+++ laus/scriptsForClasses/CONFIG_U2004/files/makeDesktopLinkForTestees.sh
@@ -4,22 +4,22 @@ if [[ $USER == testee* ]];
 then
 	if [ ! -d $HOME/Abgabe_$USER ];
 	then
-		ln -s /home/shares/schueler/Testees/Abgabe_$USER $HOME/Abgabe_$USER
+		ln -s /home/shares/testees/Abgabe_$USER $HOME/Abgabe_$USER
 	fi
 
 	if [ ! -d $HOME/Abgabe_Vorlagen ];
 	then
-		ln -s /home/shares/schueler/Testees/Vorlagen/ $HOME/Abgabe_Vorlagen
+		ln -s /home/shares/testees/Vorlagen/ $HOME/Abgabe_Vorlagen
 	fi
 
 	if [ ! -d $HOME/Schreibtisch/Abgabe_$USER ];
 	then
-		ln -s /home/shares/schueler/Testees/Abgabe_$USER $HOME/Schreibtisch/Abgabe_$USER
+		ln -s /home/shares/testees/Abgabe_$USER $HOME/Schreibtisch/Abgabe_$USER
 	fi
 
 	if [ ! -d $HOME/Schreibtisch/Vorlagen ];
 	then
-		ln -s /home/shares/schueler/Testees/Vorlagen/ $HOME/Schreibtisch/Vorlagen
+		ln -s /home/shares/testees/Vorlagen/ $HOME/Schreibtisch/Vorlagen
 	fi	
 fi
 
